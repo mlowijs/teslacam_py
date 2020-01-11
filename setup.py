1601ebc76cbc77e9a8092b8e382849a0a532b225
Michiel Lowijs <michiel.lowijs@sycade.com>
2020-01-11 09:51:14 +0100

    fix setup

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, find_namespace_packages
 from os import path
 
 here = path.abspath(path.dirname(__file__))
@@ -12,4 +12,14 @@ setup(
     description="TeslaCam uploader",
     long_description=long_description,
     long_description_content_type="text/markdown",
+    package_dir={'': 'src'},
+    packages=find_namespace_packages(where='src', include=['teslacam', 'teslacam.*']),
+    python_requires=">=3.8",
+    install_requires=[
+        "pyyaml>=5.3",
+        "azure-storage-blob>=12.1.0",
+        "sh>=1.12.14",
+        "flask>=1.1.1"
+    ],
+    scripts=['src/main.py']
 )
